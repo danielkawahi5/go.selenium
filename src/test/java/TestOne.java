188018f8dd79fba1e596599b54fcf61290a60e38
danielkawahi5 <daniel.kawa.hi5@gmail.com>
2019-12-16 18:47:27 +0100

    16.12.2019 TestOne files(1)

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,27 +1,135 @@
 import org.junit.Test;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class TestOne {
 
-    WebDriver driver;
-    private String path = "D:\\IdeaProjects\\go.selenium\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
+    private String chromeDriverPath = "D:\\IdeaProjects\\go.selenium\\src\\main\\resources\\executables\\drivers\\chromedriver.exe";
     private String chromeDriver = "webdriver.chrome.driver";
     private String baseUrl = "http://www.google.com";
-    Dimension dimension;
-    private String filesPath = "D:\\IdeaProjects\\go.selenium\\src\\main\\resources\\executables\\files";
+    Dimension dimension = new Dimension(900, 500);
+    private String filesPath = "D:\\IdeaProjects\\go.selenium\\src\\main\\resources\\executables\\files\\Test.html";
+    WebDriver driver;
+
+    public void settingProperties() {
+        System.setProperty(chromeDriver, chromeDriverPath);
+    }
 
     @Test
     public void googleOpenTest() {
-        System.setProperty(chromeDriver, path);
-        dimension = new Dimension(900, 500);
+        settingProperties();
         driver = new ChromeDriver();
         driver.manage().window().setSize(dimension);
-        driver.get(baseUrl);
-
-        driver.close();
-        System.exit(0);
+        driver.get(filesPath);
+        driver.findElement(By.id("newPage")).click();
+        driver.quit();
     }
 
+    @Test
+    public void idTest() {
+        settingProperties();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(filesPath);
+        WebElement firstNameElement = driver.findElement(By.id("fname"));
+        driver.quit();
+    }
+
+    @Test
+    public void nameTest() {
+        settingProperties();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(filesPath);
+        WebElement clickMeButton = driver.findElement(By.id("clickOnMe"));
+        WebElement clickMeButton2 = ((ChromeDriver) driver).findElementById("clickOnMe"); //bad practice
+        driver.quit();
+
+    }
+
+    @Test
+    public void textTest() {
+        settingProperties();
+        driver = new ChromeDriver();
+        driver.get(filesPath);
+        WebElement w3SchoolLink = driver.findElement(By.linkText("Visit W3Schools.com!"));
+        w3SchoolLink.click();
+        driver.navigate().back();
+        WebElement weird = driver.findElement(By.partialLinkText("Weird"));
+        weird.click();
+        driver.quit();
+    }
+
+    @Test
+    public void byClassTest() {
+        settingProperties();
+        driver = new ChromeDriver();
+        driver.get(filesPath);
+        WebElement topSecretParagraph = driver.findElement(By.className("topSecret"));
+        driver.quit();
+    }
+
+    @Test
+    public void ByHtmlTag() {
+        settingProperties();
+        driver = new ChromeDriver();
+        driver.get(filesPath);
+        WebElement firsInput = driver.findElement(By.tagName("input"));
+        WebElement firsInput1 = driver.findElement(By.tagName("a"));
+        WebElement firsInput2 = driver.findElement(By.tagName("label"));
+        driver.quit();
+    }
+
+    @Test
+    public void cssSelector() {
+        settingProperties();
+        driver = new ChromeDriver();
+        driver.get(filesPath);
+        WebElement clickOnMeCssButton = driver.findElement(By.cssSelector("#clickOnMe"));
+        WebElement clickOnMeCssButton1 = driver.findElement(By.cssSelector("a"));
+        WebElement clickOnMeCssButton2 = driver.findElement(By.cssSelector(".topSecret"));
+        WebElement tdFirstChild = driver.findElement(By.cssSelector("td:first-child"));
+    }
+
+    @Test
+    public void ebayTestCss() {
+        settingProperties();
+        driver = new ChromeDriver();
+        driver.get("http://ebay.com");
+        driver.manage().window().maximize();
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) driver;
+        driver.findElement(By.cssSelector("#gh-ac")).sendKeys("samsung");
+        driver.findElement(By.cssSelector("#gh-btn")).click();
+        javascriptExecutor.executeScript("window.scrollBy(0,900)");
+        //driver.quit();
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
